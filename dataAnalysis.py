53bb279d9d1ce5183ddbeaeb33e4e7087a34753a
DeanZ3 <dzhang1531@gmail.com>
2020-07-05 08:58:14 -0400

    jean s folder

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -91,32 +91,3 @@ print("lol")
 #print(arrayAllFiles)
 
 #************************************************************************************
-df = pd.read_csv('mpg.csv')
-df = df.replace('?', np.nan)
-df = df.dropna()
-
-df = df.drop(['name','origin','model_year'], axis=1)
-X = df.drop('mpg', axis=1)
-y = df[['mpg']]
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
-
-reg = LinearRegression()
-reg.fit(X_train[['horsepower']], y_train)
-
-y_predicted = reg.predict(X_test[['horsepower']])
-print("Mean squared error: %.2f" % mean_squared_error(y_test, y_predicted))
-print('R²: %.2f' % r2_score(y_test, y_predicted))
-
-reg = LinearRegression()
-reg.fit(X_train[['horsepower','weight','cylinders']], y_train)
-y_predicted = reg.predict(X_test[['horsepower','weight','cylinders']])
-print("Mean squared error: %.2f" % mean_squared_error(y_test, y_predicted))
-print('R²: %.2f' % r2_score(y_test, y_predicted))
-
-fig, ax = plt.subplots()
-ax.scatter(y_test, y_predicted)
-ax.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'k--', lw=4)
-ax.set_xlabel('measured')
-ax.set_ylabel('predicted')
-plt.show()
